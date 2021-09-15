3ee63b0e630ae93be567cd67e208a0f6e26bf117
Dan Bradham <danielbradham@gmail.com>
2021-09-15 11:12:58 -0400

    Add library metadata.

--- review4d/__init__.py
+++ review4d/__init__.py
@@ -9,6 +9,13 @@ from .postrender import *
 from .ui import *
 
 
+__author__ = 'Brand New School, Dan Bradham'
+__email__ = 'dan@brandnewschool.com'
+__license__ = 'MIT'
+__version__ = '0.1.1'
+version_info = tuple([int(i) for i in __version__.split('.')])
+
+
 def show_render_dialog():
     import c4d
     c4d.CallCommand(RENDER_COMMAND_ID)
